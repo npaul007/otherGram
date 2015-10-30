cdbb35f110bf9b75333df2be6b9b3637a68b0a12
Nate <npaulemon@gmail.com>
2015-10-30 12:29:58 -0400

    design formatted 2

---
+++ client/layout/layout.js
@@ -0,0 +1,11 @@
+Template.layout.events({
+	'click .fa-sign-out':function(){
+		Router.go('/');
+	},
+	'click .fa-globe':function(){
+		Router.go('/worldPics');
+	},
+	'click .fa-home':function(){
+		Router.go('/yourPics');
+	}
+});
