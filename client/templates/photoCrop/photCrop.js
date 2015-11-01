8e8fe97805ca38454574e28b98acb26bc7c023e3
Nate <npaulemon@gmail.com>
2015-11-01 10:48:23 -0500

    comit before merge

--- client/templates/photoCrop/photCrop.js
+++ client/templates/photoCrop/photCrop.js
@@ -5,16 +5,21 @@ Images = new FS.Collection("images",{
 Template.photoCrop.events({
 	'click #submitPic':function(event,template){
 		event.preventDefault();
+
 		var file = $('#uploaded').get(0).files[0]; // Stores temporaly the FSFile
 		var fsFile = new FS.File(file); // take the FS.File object
+
 		var textareaText = $('#text').val()
+
 		fsFile.metadata = {
 			post:textareaText,
 			likes:0,
 			userId:Meteor.userId(),
 			username:Meteor.user().username
 		};
+
 		Images.insert(fsFile);
+		
 		Router.go('/worldPics');
 	}
 });
