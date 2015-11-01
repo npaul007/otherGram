179f5216c0893c87d18b371c738811cbdfd10a47
Nate <npaulemon@gmail.com>
2015-11-01 11:29:37 -0500

    likes added 1

--- client/templates/worldPics/worldPics.js
+++ client/templates/worldPics/worldPics.js
@@ -4,3 +4,17 @@ Template.worldPics.helpers({
  		return Images.find() ;
  	}
 });
+
+Template.worldPics.rendered=function(){
+	 if (!this.rendered){
+	    $('#spinner').show();
+	  }else{
+	  	$('#spinner').hide();
+	  }
+}
+
+Template.worldPics.events({
+ 	'click .fa-thumbs-o-up':function(){
+ 		Images.update({_id:this._id},{$inc:{"metadata.likes":1}});
+ 	}
+});
