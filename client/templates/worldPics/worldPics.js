b80eeda44b72356613de8cebf1f496aabe830049
Nate <npaulemon@gmail.com>
2015-11-02 22:38:57 -0500

    router secure

--- client/templates/worldPics/worldPics.js
+++ client/templates/worldPics/worldPics.js
@@ -1,17 +1,8 @@
-
 Template.worldPics.helpers({
  	'images': function(){
  		return Images.find({},{sort:{"copies.images.updatedAt":-1}});
  	}
 });
-
-Template.worldPics.rendered=function(){
-	 if (!this.rendered){
-	    $('#spinner').show();
-	  }else{
-	  	$('#spinner').hide();
-	  }
-}
 
 Template.worldPics.events({
  	'click .fa-thumbs-o-up':function(){
