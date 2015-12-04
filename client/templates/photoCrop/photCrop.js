8cc75240ef220314186d78989d4c3ed4034db764
Nate <npaulemon@gmail.com>
2015-12-04 17:22:08 -0500

    remove comments

--- client/templates/photoCrop/photCrop.js
+++ client/templates/photoCrop/photCrop.js
@@ -41,8 +41,8 @@ Template.photoCrop.events({
 	'click #submitPic':function(event,template){
 		event.preventDefault();
 
-		var file = $('#uploaded').get(0).files[0]; // Stores temporaly the FSFile
-		var fsFile = new FS.File(file); // take the FS.File object
+		var file = $('#uploaded').get(0).files[0]; 
+		var fsFile = new FS.File(file);
 
 		var textareaText = $('#text').val()
 
