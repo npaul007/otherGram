28f90ef3ec8c4fe0c2eda19a78d63c6be65bd6d9
Nate <npaulemon@gmail.com>
2015-12-06 21:33:41 -0500

    loading spinner for bottom navbar 2

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -22,3 +22,8 @@ Template.navbar.helpers({
 Template.registerHelper("isCurrentPage",function(current){
 	return current === Session.get("currentRouteName") ;
 });
+
+Template.footer.onCreated(function () {
+  // Use this.subscribe inside onCreated callback
+  this.subscribe("currentUser");
+});
