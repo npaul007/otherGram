814026cd693276d61e9660805289fc5f14a53cf4
Nate <npaulemon@gmail.com>
2016-01-04 20:53:58 -0500

    storing respective user id in session variable when clicked

--- client/templates/people/people.js
+++ client/templates/people/people.js
@@ -1,7 +1,18 @@
-Meteor.subscribe('accounts');
+if(Meteor.isClient){
+	Meteor.subscribe('accounts');
+}
 
 Template.people.helpers({
 	'users':function(){
 		return Meteor.users.find();
 	}
 })
+
+Template.people.events({
+	'click #userDiv':function(event,template){
+		event.preventDefault();
+		var selectedUserId = this._id;
+		Session.set('currentUser',selectedUserId);
+		console.log(Session.get('currentUser'));
+	}
+});
