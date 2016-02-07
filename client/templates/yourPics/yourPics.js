4e59b3d82dbcebf53bbe5190e50597ac7c44ffdc
Nate <npaulemon@gmail.com>
2016-02-07 14:12:43 -0500

    comment input for yourPics fixed, forgot to add idGen function

--- client/templates/yourPics/yourPics.js
+++ client/templates/yourPics/yourPics.js
@@ -57,6 +57,14 @@ Template.yourPics.rendered = function(){
 	}
 }
 
+// id generator
+function guidGenerator() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
 Template.yourPics.events({
 	'click .fa-pencil-square-o':function(event,template){
 		event.preventDefault();
