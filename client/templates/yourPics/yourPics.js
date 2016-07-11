28942359605da24ecf47e52f86c4e3bdd76cc060
Nate <npaulemon@gmail.com>
2016-07-10 20:46:36 -0400

    yourpics now server methods added

--- client/templates/yourPics/yourPics.js
+++ client/templates/yourPics/yourPics.js
@@ -107,23 +107,7 @@ Template.yourPics.events({
 
 		if(Meteor.userId() === userId || Meteor.userId() === ownersUserId || Meteor.user().profile.type === 'admin'){
 			if(confirm("Would you like to delete this comment?")){
-				Images.update(
-					{
-						_id:imageId
-					} , 
-					{
-						$pull:
-						{
-							"metadata.comments":
-							{
-								"_id":commentId,
-								"userId":userId,
-								"username":username, 
-								"comment":comment
-							}
-						}
-					}
-				);
+				Meteor.call('removeComment',imageId,commentId,userId,username,comment);
 			}else{
 				if(Meteor.userId() == userId){
 					if(confirm("Would you like to edit this comment?")){
@@ -137,34 +121,7 @@ Template.yourPics.events({
 	// like button
 	'click .fa-thumbs-o-up':function(event){
 		event.preventDefault();
-		
-		// if the user has already likes this photo
-		if(Images.find(
-			{
-				$and: [
-					{_id:this._id}, 
-					{
-						"metadata.likes":
-						{
-							$elemMatch:
-							{
-								"userId":Meteor.userId()
-							}
-						}
-					}
-				]
-			}
-			).count() > 0){
-			// offer the option to unlike it
-			if(confirm("You have already liked this photo, would you like to unlike this photo?")){
-				Images.update({_id:this._id}, {$pull:{"metadata.likes":{"userId":Meteor.userId()}}});
-			}else{
-				return false; // if they say no do nothing
-			}
-		}else{
-			// if they havent liked the photo, like it
-			Images.update({_id:this._id}, {$push:{"metadata.likes":{"userId":Meteor.userId()}}});
-		}
+		Meteor.call('likePicture',this._id);
 	},
 	'click .pPic':function(){
 		// create sessions variable of selected photo to be displayed in selectedPicture template
@@ -174,18 +131,12 @@ Template.yourPics.events({
 		Session.set('previousPage','yourPics');
 
 		Router.go('/selectedPicture');
-	}
-	,
- 	'dblclick .picDiv':function(){
- 		if(Meteor.user().profile.type === 'admin'){
- 			Images.remove({_id:this._id});
- 		}
- 	},
+	},
  	'click #deletePostButton':function(){
  		if(Meteor.userId() === this.metadata.userId || Meteor.user().profile.type === 'admin'){
 	 		var del = confirm("Are you sure you want to delete this picture?");
 	 		if(del){
-	 			Images.remove({_id:this._id});
+	 			Meteor.call('removeImage',this._id, this.metadata.userId);
 	 		}
  		}
  	},
@@ -208,22 +159,7 @@ Template.yourPics.events({
  			if(comment.length == 0){
  				return;
  			}else{
-	 			Images.update(
-	 				{
-	 					_id:this._id
-	 				} , 
-	 				{
-	 					$push:{
-	 						"metadata.comments":
-	 						{
-	 							"_id":guidGenerator(),
-	 							"userId":Meteor.userId(),
-	 							"username":Meteor.user().username ,
-	 							"comment":comment
-	 						}
-	 					}
-	 				}
-	 			);
+				Meteor.call('insertComment',this._id,guidGenerator(),comment);
 				event.target.value = "";
  			}
  		}
